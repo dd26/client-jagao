40a4419017878e37d8e77c390a224e84cf67b298
example <denilsson.d.sousa@gmail.com>
2022-03-02 13:44:07 -0400

    splash, axios, colores, env

---
+++ src/pages/Splash.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="absolute-full flex flex-center">
+    <img src="logos/logo1.svg" alt="logo_jagao_1">
+    <div
+      class="text-uppercase text-bold text-primary fixed-bottom full-width text-center q-pb-xl"
+      style="font-size: 20px;"
+    >Welcome</div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      timer: ''
+    }
+  },
+  beforeDestroy () {
+    clearInterval(this.timer)
+  },
+  mounted () {
+    this.timer = setInterval(this.redirectToLogin, 4000)
+  },
+  methods: {
+    redirectToLogin () {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+section {
+  background: linear-gradient(192.12deg, rgba(0, 167, 142, 0.15) 0.44%, rgba(0, 167, 142, 0.124536) 47.5%, rgba(0, 167, 142, 0) 99.21%);
+}
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,28 @@
 
 const routes = [
   {
+    path: '/login',
+    component: () => import('pages/Login.vue')
+  },
+  {
     path: '/',
+    component: () => import('pages/Splash.vue')
+  },
+  {
+    path: '/home',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
+  {
+    path: '/',
+    redirect: '/splash'
+  },
+  {
+    path: '*',
+    redirect: '/login'
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
