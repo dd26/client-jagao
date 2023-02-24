3ac40d682ccb5205038cc7b7f508320a2e70eb15
Denilsson De Sousa <denilsson.d.sousa@gmail.com>
2023-02-23 23:07:37 -0400

    deploy jagao

--- src/env-example.js
+++ src/env-example.js
@@ -1,6 +1,3 @@
-/*
-* Archivo de ejemplo para el env.js. Cópialo y renómbralo a env.js
-*/
 export default {
-  apiUrl: 'http://localhost:3333/api/'
+  apiUrl: 'https://jagao.dev/backend/public/api/'
 }
