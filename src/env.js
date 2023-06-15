a14a0bbe433b701a71a349284615bc6abfc3f5e9
Denilsson De Sousa <denilsson.d.sousa@gmail.com>
2023-06-15 10:40:44 -0400

    cambuioo

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 export default {
-  // apiUrl: 'https://jagao.dev/backend/public/api/',
-  apiUrl: 'http://localhost:8000/api/',
+  apiUrl: 'https://jagao.co/back_jagao/public/api/',
+  // apiUrl: 'http://localhost:8000/api/',
   // apiUrl: '',
   googleApiKey: 'AIzaSyAXFQx7ndE8QQX5f4H1UbbSAzlhgcwYCHY'
 }
