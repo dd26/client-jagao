469ad5abb31830e0d6072b3af6f9bfcc2d7a1470
Denilsson De Sousa <denilsson.d.sousa@gmail.com>
2023-06-16 13:36:00 -0400

    cambios pantalla notificaciones ios y android

--- src/pages/employee/Notifications.vue
+++ src/pages/employee/Notifications.vue
@@ -10,13 +10,25 @@
       />
       <div class="text-primary" style="font-size: 20px; font-weight: 700;">Notifications</div>
     </section>
-    <div class="q-px-sm row q-gutter-y-lg">
+    <div
+      class="q-px-sm row q-gutter-y-lg"
+      v-if="data.length > 0"
+    >
       <EmployeeItem
         v-for="n in data"
         :key="n.id"
         v-bind="n"
         class="col-12"
       />
+    </div>
+    <div
+      class="q-px-sm"
+      v-else
+    >
+      <div class="text-center text-grey absolute-center" style="font-size: 20px;">
+        <!-- No posee notificaciones en este momento -->
+        No notifications at this time
+      </div>
     </div>
   </q-page>
 </template>
